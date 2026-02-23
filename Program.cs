fe4a2a4f062ffa6187482d4697b915d75fd5a6a0
nrfx <zavovaz001@gmail.com>
2026-02-23 17:42:50 +0800

    скелетик проверки условия сделал

--- Program.cs
+++ Program.cs
@@ -6,4 +6,12 @@ string word1 = Console.ReadLine();
 Console.Write("Введите второе слово: ");
 string word2 = Console.ReadLine();
 
+string checkedLttrs = "";
 
+foreach(char letter in word1) // перебираю побуквенно первое слово 
+{
+    if (checkedLttrs.Contains(letter) == false) // если в переменной проверенных букв нет буквы то добавляем
+    {
+      checkedLttrs += letter;
+    }
+}
